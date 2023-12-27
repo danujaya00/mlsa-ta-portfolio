0d0af0b4b0fe858ef7b76a17febfe69e833fa261
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2023-12-27 17:49:53 +0000

    change details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Danujaya Liyanage",
+  title: "Developer & Designer",
+  email: "Danujayas00@gmail.com",
+  gitHub: "danujaya00",
+  instagram: "_dee00.__",
+  linkedIn: "danujayaliyanage",
+  medium: "danujaya.sandeepa",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
